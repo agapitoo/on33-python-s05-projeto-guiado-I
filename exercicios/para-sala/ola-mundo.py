9e5f1dcaacb5796b0d0efb9e7db72bc92f253f75
agapitoo <fernandaagapito2013@gmail.com>
2024-07-04 22:12:58 -0300

    04/07

---
+++ exercicios/para-sala/ola-mundo.py
@@ -0,0 +1,9 @@
+import print_reprograma as pr    
+
+pr.cor('magenta')
+pr.limpar_formatacao()
+pr.imprimir('ola, mundo ')
+pr.separador(60, cor_texto='azul' )
+pr.retangulo('Texto dentro de um retangulo', cor_barra='amarelo', cor_texto='vermelho')
+pr.limpar_formatacao()
+
